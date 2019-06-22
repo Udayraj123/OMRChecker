f3bbf38f2bca8e5a622c13cd5e63df7630c01916
udayraj123 <rhuge123@gmail.com>
2019-06-23 03:22:00 +0530

    script n conversion complete

--- extras/mini_scripts/tiftojpg.py
+++ extras/mini_scripts/tiftojpg.py
@@ -7,17 +7,16 @@ https://github.com/Udayraj123
 """
 import os
 import glob
-import cv2
 # review = 1
 review = 0
 # backupOMRs\Agra_1057\HE\Normal>Agra_HE_0054.tif
-dir_glob ='backupOMRs\\*\\*\\*'
+dir_glob ='backupOMRs/*/*/*'
 counter = 0
 for x in glob.iglob(dir_glob):
 	try:
 		for filename in os.listdir(x):
 			if filename.endswith(".tif"):
-				filepath = x+"\\"+filename
+				filepath = x+"/"+filename
 				counter += 1
 				if(review):
 					print(counter, filepath)
@@ -27,13 +26,14 @@ for x in glob.iglob(dir_glob):
 						print("Making dirs:",newx)
 						os.makedirs(newx)
 					else:
-						print("Already exists:",newx)
+						print("Dir already exists:",newx)
 
-					newfilepath = newx +filename[:-4]+".jpg"
-					cv2.imwrite(newfilepath,cv2.imread(filepath))
-					# print("System cmd:",os.system(R'"C:\Program Files\ImageMagick-7.0.8-Q16\convert.exe" '+filepath+' '+newfilepath))
-					print(counter, newfilepath)
-				exit(0)
+					newfilepath = newx +"/"+filename[:-4]+".jpg"
+					#  -resize 50%
+					if(os.system("convert "+filepath+' '+newfilepath)==0):
+						print(counter, newfilepath)
+					else:
+						print(counter,"Failed to convert: ", filename)
 	except NotADirectoryError:
 		pass
 
