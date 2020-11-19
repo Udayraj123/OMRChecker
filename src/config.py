b2245a3cd90d556fbb6070c156bdcce8afb4d660
udayraj <rhuge123@gmail.com>
2020-11-19 20:30:07 +0530

    fix: conflicts

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 from .constants import CONFIG_DEFAULTS_PATH
 from .utils.file import loadJson
-from .utils.object import override_merger
+from .utils.object import OVERRIDE_MERGER
 from dotmap import DotMap
 
 configDefaults = DotMap(loadJson(CONFIG_DEFAULTS_PATH))
@@ -8,5 +8,5 @@ configDefaults = DotMap(loadJson(CONFIG_DEFAULTS_PATH))
 
 def openConfigWithDefaults(configPath):
     user_config = loadJson(configPath)
-    merged_dict = override_merger.merge(configDefaults, user_config)
+    merged_dict = OVERRIDE_MERGER.merge(configDefaults, user_config)
     return DotMap(merged_dict)
--- src/processors/CropOnMarkers.py
+++ src/processors/CropOnMarkers.py
@@ -6,45 +6,47 @@ import numpy as np
 import src.utils.notSorted as utils
 from src.config import configDefaults as config
 
+
 class CropOnMarkers(ImagePreprocessor):
     def __init__(self, marker_ops, cwd):
         self.thresholdCircles = []
+        # img_utils = ImageUtils()
+
         # options with defaults
         self.marker_path = os.path.join(
-            cwd, marker_ops.get("relativePath", "omr_marker.jpg"))
+            cwd, marker_ops.get("relativePath", "omr_marker.jpg")
+        )
         self.minMatchingThreshold = marker_ops.get("minMatchingThreshold", 0.3)
-        self.maxMatchingVariation = marker_ops.get(
-            "maxMatchingVariation", 0.41)
-        self.marker_rescale_range = marker_ops.get(
-            "marker_rescale_range", (35, 100))
+        self.maxMatchingVariation = marker_ops.get("maxMatchingVariation", 0.41)
+        self.marker_rescale_range = marker_ops.get("marker_rescale_range", (35, 100))
         self.marker_rescale_steps = marker_ops.get("marker_rescale_steps", 10)
         self.apply_erode_subtract = marker_ops.get("apply_erode_subtract", 1)
-        if(not os.path.exists(self.marker_path)):
+        if not os.path.exists(self.marker_path):
             print(
                 "Error: Marker not found at path provided in template:",
-                self.marker_path)
+                self.marker_path,
+            )
             exit(31)
 
         marker = cv2.imread(self.marker_path, cv2.IMREAD_GRAYSCALE)
 
-        if("sheetToMarkerWidthRatio" in marker_ops):
+        if "sheetToMarkerWidthRatio" in marker_ops:
             # TODO: processing_width should come through proper channel
-            marker = utils.resize_util(marker, config.dimensions.processing_width /
-                                    int(marker_ops["sheetToMarkerWidthRatio"]))
+            marker = utils.resize_util(
+                marker,
+                config.dimensions.processing_width
+                / int(marker_ops["sheetToMarkerWidthRatio"]),
+            )
         marker = cv2.GaussianBlur(marker, (5, 5), 0)
         marker = cv2.normalize(
-            marker,
-            None,
-            alpha=0,
-            beta=255,
-            norm_type=cv2.NORM_MINMAX)
+            marker, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX
+        )
 
-        if(self.apply_erode_subtract):
+        if self.apply_erode_subtract:
             # TODO: verify its effectiveness in practical cases
             marker -= cv2.erode(marker, kernel=np.ones((5, 5)), iterations=5)
-        
+
         self.marker = marker
-        
 
     def __str__(self):
         return self.marker_path
@@ -57,42 +59,48 @@ class CropOnMarkers(ImagePreprocessor):
     def getBestMatch(self, image_eroded_sub):
 
         descent_per_step = (
-            self.marker_rescale_range[1] - self.marker_rescale_range[0]) // self.marker_rescale_steps
+            self.marker_rescale_range[1] - self.marker_rescale_range[0]
+        ) // self.marker_rescale_steps
         h, w = self.marker.shape[:2]
         res, best_scale = None, None
         allMaxT = 0
 
         for r0 in np.arange(
-                self.marker_rescale_range[1], self.marker_rescale_range[0], -1 * descent_per_step):  # reverse order
+            self.marker_rescale_range[1],
+            self.marker_rescale_range[0],
+            -1 * descent_per_step,
+        ):  # reverse order
             s = float(r0 * 1 / 100)
-            if(s == 0.0):
+            if s == 0.0:
                 continue
-            rescaled_marker = utils.resize_util_h(self.marker,
-                u_height=int(
-                    h * s))
+            rescaled_marker = utils.resize_util_h(self.marker, u_height=int(h * s))
             # res is the black image with white dots
             res = cv2.matchTemplate(
-                image_eroded_sub,
-                rescaled_marker,
-                cv2.TM_CCOEFF_NORMED)
+                image_eroded_sub, rescaled_marker, cv2.TM_CCOEFF_NORMED
+            )
 
             maxT = res.max()
-            if(allMaxT < maxT):
+            if allMaxT < maxT:
                 # print('Scale: '+str(s)+', Circle Match: '+str(round(maxT*100,2))+'%')
                 best_scale, allMaxT = s, maxT
 
-        if(allMaxT < self.minMatchingThreshold):
-            print("\tWarning: Template matching too low! Consider rechecking preProcessors applied before this.")
-            if(config.outputs.show_image_level >= 1):
+        if allMaxT < self.minMatchingThreshold:
+            print(
+                "\tWarning: Template matching too low! Consider rechecking preProcessors applied before this."
+            )
+            if config.outputs.show_image_level >= 1:
                 show("res", res, 1, 0)
 
-        if(best_scale is None):
+        if best_scale is None:
             print("No matchings for given scaleRange:", self.marker_rescale_range)
         return best_scale, allMaxT
 
     def apply_filter(self, image, args):
-        image_eroded_sub = utils.normalize_util(image if self.apply_erode_subtract else (
-            image - cv2.erode(image, kernel=np.ones((5, 5)), iterations=5)))
+        image_eroded_sub = utils.normalize_util(
+            image
+            if self.apply_erode_subtract
+            else (image - cv2.erode(image, kernel=np.ones((5, 5)), iterations=5))
+        )
         # Quads on warped image
         quads = {}
         h1, w1 = image_eroded_sub.shape[:2]
@@ -104,18 +112,19 @@ class CropOnMarkers(ImagePreprocessor):
         quads[3] = image_eroded_sub[midh:h1, midw:w1]
 
         # Draw Quadlines
-        image_eroded_sub[:, midw:midw + 2] = 255
-        image_eroded_sub[midh:midh + 2, :] = 255
+        image_eroded_sub[:, midw : midw + 2] = 255
+        image_eroded_sub[midh : midh + 2, :] = 255
 
         best_scale, allMaxT = self.getBestMatch(image_eroded_sub)
-        if(best_scale is None):
+        if best_scale is None:
             # TODO: Plot and see performance of marker_rescale_range
-            if(config.outputs.show_image_level >= 1):
-                utils.show('Quads', image_eroded_sub)
+            if config.outputs.show_image_level >= 1:
+                utils.show("Quads", image_eroded_sub)
             return None
 
-        optimal_marker = utils.resize_util_h(self.marker, u_height=int(
-                self.marker.shape[0] * best_scale))
+        optimal_marker = utils.resize_util_h(
+            self.marker, u_height=int(self.marker.shape[0] * best_scale)
+        )
         h, w = optimal_marker.shape[:2]
         centres = []
         sumT, maxT = 0, 0
@@ -124,11 +133,14 @@ class CropOnMarkers(ImagePreprocessor):
             res = cv2.matchTemplate(quads[k], optimal_marker, cv2.TM_CCOEFF_NORMED)
             maxT = res.max()
             print("Q" + str(k + 1) + ": maxT", round(maxT, 3), end="\t")
-            if(maxT < self.minMatchingThreshold or abs(allMaxT - maxT) >= self.maxMatchingVariation):
+            if (
+                maxT < self.minMatchingThreshold
+                or abs(allMaxT - maxT) >= self.maxMatchingVariation
+            ):
                 # Warning - code will stop in the middle. Keep Threshold low to
                 # avoid.
                 print(
-                    args['current_file'].name,
+                    args["current_file"].name,
                     "\nError: No circle found in Quad",
                     k + 1,
                     "\n\tminMatchingThreshold",
@@ -138,10 +150,13 @@ class CropOnMarkers(ImagePreprocessor):
                     "\tmaxT",
                     maxT,
                     "\tallMaxT",
-                    allMaxT)
-                if(config.outputs.show_image_level >= 1):
-                    utils.show("no_pts_" + args['current_file'].name, image_eroded_sub, 0)
-                    utils.show("res_Q" + str(k + 1) + " ("+str(maxT)+")", res, 1)
+                    allMaxT,
+                )
+                if config.outputs.show_image_level >= 1:
+                    utils.show(
+                        "no_pts_" + args["current_file"].name, image_eroded_sub, 0
+                    )
+                    utils.show("res_Q" + str(k + 1) + " (" + str(maxT) + ")", res, 1)
                 return None
 
             pt = np.argwhere(res == maxT)[0]
@@ -149,21 +164,17 @@ class CropOnMarkers(ImagePreprocessor):
             pt[0] += origins[k][0]
             pt[1] += origins[k][1]
             # print(">>",pt)
-            image = cv2.rectangle(image, tuple(
-                pt), (pt[0] + w, pt[1] + h), (150, 150, 150), 2)
+            image = cv2.rectangle(
+                image, tuple(pt), (pt[0] + w, pt[1] + h), (150, 150, 150), 2
+            )
             # display:
             image_eroded_sub = cv2.rectangle(
                 image_eroded_sub,
                 tuple(pt),
-                (pt[0] + w,
-                pt[1] + h),
-                (50,
-                50,
-                50) if self.apply_erode_subtract else (
-                    155,
-                    155,
-                    155),
-                4)
+                (pt[0] + w, pt[1] + h),
+                (50, 50, 50) if self.apply_erode_subtract else (155, 155, 155),
+                4,
+            )
             centres.append([pt[0] + w / 2, pt[1] + h / 2])
             sumT += maxT
         print("Optimal Scale:", best_scale)
@@ -180,12 +191,17 @@ class CropOnMarkers(ImagePreprocessor):
         # res[ : , midw:midw+2] = 255
         # res[ midh:midh+2, : ] = 255
         # show("Markers Matching",res)
-        if(config.outputs.show_image_level >= 2 and config.outputs.show_image_level < 4):
+        if config.outputs.show_image_level >= 2 and config.outputs.show_image_level < 4:
             image_eroded_sub = utils.resize_util_h(image_eroded_sub, image.shape[0])
             image_eroded_sub[:, -5:] = 0
             h_stack = np.hstack((image_eroded_sub, image))
-            utils.show("Warped: " + args['current_file'].name, utils.resize_util(h_stack,
-                                                        int(config.display_width * 1.6)), 0, 0, [0, 0])
+            utils.show(
+                "Warped: " + args["current_file"].name,
+                utils.resize_util(h_stack, int(config.display_width * 1.6)),
+                0,
+                0,
+                [0, 0],
+            )
         # iterations : Tuned to 2.
         # image_eroded_sub = image_norm - cv2.erode(image_norm, kernel=np.ones((5,5)),iterations=2)
         return image
