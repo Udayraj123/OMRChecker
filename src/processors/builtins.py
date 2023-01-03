5ef74f52af6e4efc47f7656b357e883bd5ebc3de
rudrapsc <114874767+rudrapsc@users.noreply.github.com>
2023-01-03 20:43:36 +0530

    Update builtins.py

--- src/processors/builtins.py
+++ src/processors/builtins.py
@@ -49,38 +49,3 @@ class GaussianBlur(ImagePreprocessor):
             tuple(self.options.get("kSize", (3, 3))),
             self.options.get("sigmax", 0),
         )
-
-
-class ReadBarcode(ImagePreprocessor):
-
-    def __init__(self, options, _args):
-        self.x1 = options.get("x1")
-        self.x2 = options.get("x2")
-        self.y1 = options.get("y1")
-        self.y2 = options.get("y2")
-
-    def apply_filter(self,img,args,save_dir):
-        img1=img[self.x1 : self.x2 ,self.y1 : self.y2]
-        cv2.imshow("cropped", img1)
-
-        def detect(image):
-            # image = cv2.resize(image, (5000, 5000))
-            for barcode in decode(image):
-                data = barcode.data.decode('utf-8')
-                return data
-
-        def make_folders(path, data):
-            b=0
-            for file in os.listdir(path):
-                if (file == str(data)):
-                    break
-            else:
-                os.mkdir(path+'/'+str(data))
-
-        data=detect(img1)
-        if data== None:
-            data='error'
-        path=str(save_dir[:-1])
-        logger.info(path)
-        make_folders(path,data)
-        return str(data) +'/'
