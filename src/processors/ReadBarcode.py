44c68d1ee53d1a7a84a5cc0d2518c94b04824a58
rudrapsc <114874767+rudrapsc@users.noreply.github.com>
2023-01-06 03:20:14 +0530

    Update ReadBarcode.py

--- src/processors/ReadBarcode.py
+++ src/processors/ReadBarcode.py
@@ -14,6 +14,9 @@ class ReadBarcode(ImagePreprocessor):
         self.x2 = options.get("x2")
         self.y1 = options.get("y1")
         self.y2 = options.get("y2")
+        self.qr_to_output=options.get("qr_to_output_directory")
+        self.output_sorting=options.get("output_sorting",False)
+        logger.info(self.qr_to_output)
 
     def apply_filter(self,img,args,save_dir):
         img1=img[self.x1 : self.x2 ,self.y1 : self.y2]
@@ -36,6 +39,10 @@ class ReadBarcode(ImagePreprocessor):
         if data== None:
             data='error'
         path=str(save_dir[:-1])
-        logger.info(path)
-        make_folders(path,data)
-        return str(data) +'/'
+        # logger.info(self.qr_to_output[data])
+        if self.qr_to_output is not None:
+            data_1=self.qr_to_output[data]
+        else:
+            data_1=data
+        make_folders(path,data_1)
+        return str(data_1) +'/',self.output_sorting
