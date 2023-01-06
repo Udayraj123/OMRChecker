abbcacd0d89e8334e4e60508d3942326585b7721
rudrapsc <114874767+rudrapsc@users.noreply.github.com>
2023-01-06 20:37:17 +0530

    Update ReadBarcode.py

--- src/processors/ReadBarcode.py
+++ src/processors/ReadBarcode.py
@@ -16,17 +16,21 @@ class ReadBarcode(ImagePreprocessor):
         self.y2 = options.get("y2")
         self.qr_to_output=options.get("qr_to_output_directory")
         self.output_sorting=options.get("output_sorting",False)
-        logger.info(self.qr_to_output)
+        self.input_sorting=options.get("input_sorting",False)
 
     def apply_filter(self,img,args,save_dir):
         img1=img[self.x1 : self.x2 ,self.y1 : self.y2]
         cv2.imshow("cropped", img1)
+        cv2.waitKey(0)
 
         def detect(image):
             # image = cv2.resize(image, (5000, 5000))
-            for barcode in decode(image):
+            b=0
+            data=None
+            for i,barcode in enumerate(decode(image)):
                 data = barcode.data.decode('utf-8')
-                return data
+                b=i+1
+            return data,b
 
         def make_folders(path, data):
             for file in os.listdir(path):
@@ -35,14 +39,24 @@ class ReadBarcode(ImagePreprocessor):
             else:
                 os.mkdir(path+'/'+str(data))
 
-        data=detect(img1)
+        data,size=detect(img1)
+        if size>1:
+            logger.error(
+                "\tError: Multiple QR found, size=",size
+            )
+            data=''
         if data== None:
-            data='error'
+            logger.error(
+                "\tError: QR not found :Have you accidentally included ReadBarcode plugin?"
+            )
+            data=''
         path=str(save_dir[:-1])
-        # logger.info(self.qr_to_output[data])
         if self.qr_to_output is not None:
             data_1=self.qr_to_output[data]
         else:
             data_1=data
         make_folders(path,data_1)
-        return str(data_1) +'/',self.output_sorting
+        if self.input_sorting:
+            data_2=data_1+'_save'
+            make_folders(path,data_2)
+        return str(data_1) +'/',self.input_sorting
