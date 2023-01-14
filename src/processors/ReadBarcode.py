a4f0cf03297c6fc3ceee2abf7b66360a37124d5d
Rudra <114874767+rudrapsc@users.noreply.github.com>
2023-01-14 20:34:12 +0530

    Update ReadBarcode.py

--- src/processors/ReadBarcode.py
+++ src/processors/ReadBarcode.py
@@ -13,32 +13,29 @@ class ReadBarcode(ImagePreprocessor):
         self.y1 = options.get("y1")
         self.y2 = options.get("y2")
         self.qr_to_output = options.get("qr_to_output_directory")
-        self.output_sorting = options.get("output_sorting", False)
         self.input_sorting = options.get("input_sorting", False)
 
-    def apply_filter(self, img, args):
-        img1 = img[self.x1 : self.x2, self.y1 : self.y2]
-        cv2.imshow("cropped", img1)
-        cv2.waitKey(0)
 
-        def detect(image):
-            # image = cv2.resize(image, (5000, 5000))
-            b = 0
-            data = None
-            for i, barcode in enumerate(decode(image)):
-                data = barcode.data.decode("utf-8")
-                b = i + 1
-            return data, b
-
-        data, size = detect(img1)
+    def detect(image):
+        size = 0
+        data = None
+        for i, barcode in enumerate(decode(image)):
+            data = barcode.data.decode("utf-8")
+            size = i + 1
         if size > 1:
             logger.error("\tError: Multiple QR found, size=", size)
-            data = ""
-        if data is None:
+            data = None
+        elif data is None:
             logger.error(
                 "\tError: QR not found :Have you accidentally included ReadBarcode plugin?"
             )
-            data = ""
+            data="error"
+        return data
+
+    def apply_filter(self, img, args):
+        img1 = img[self.x1 : self.x2, self.y1 : self.y2]
+
+        data = ReadBarcode.detect(img1)
         if self.qr_to_output is not None:
             data_1 = self.qr_to_output[data]
         else:
